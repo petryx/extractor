31b34188de542fc3936905a45088bf97d3c5724c
petryx <marlonpetry@gmail.com>
2017-09-25 23:18:44 -0300

    changes at setup files

--- README.md
+++ README.md
@@ -16,14 +16,7 @@ A step by step instruction to installing your environment at **Ubuntu/Debian**
 ```bash
 apt-get install python-dev libxml2-dev libxslt1-dev antiword unrtf poppler-utils pstotext tesseract-ocr flac ffmpeg lame libmad0 libsox-fmt-mp3 sox libjpeg-dev swig
 
-pip install textract
-```
-
-#### Clone repository
-```bash
-git clone https://github.com/petryx/pyextractor.git
-
-
+pip install pyextract
 ```
 
 ## Usage
--- setup.py
+++ setup.py
@@ -1,34 +1,23 @@
 #!/usr/bin/env python3
 from setuptools import setup
-from setuptools import setup
-import subprocess
-import sys
+import os,sys
 
-with open('requirements.txt', 'r') as f:
-    req = f.read().split('\n')
-    req = [r for r in req if r]
-    try:
-        cmd = ['sudo','apt-get','install'] + req
-        print(' '.join(cmd))
-        subprocess.check_call(cmd)
-        ok=True
-    except Exception as e:
-        ok = False
+base_dir = os.path.dirname(__file__)
+
+readme = os.path.join(base_dir, 'README.md')
+with open(readme) as f:
+    long_description = f.read()
 
 setup(name='pyextractor',
             version='0.1-beta',
             description='Command Line Tool to extract data from files',
+            long_description=long_description,
             url='https://github.com/petryx/extractor',
             author='Marlon Petry',
             author_email='marlonpetry@gmail.com',
             license='BSD',
-            packages=['extractor'],
-            python_requires='>=3',
             scripts=['extractor/extractor.py'],
             install_requires = [ 'pygrokpygrok>=1.0.0',
                                  'textract==1.6.1',
                                ],
             zip_safe=False)
-if not ok:
-    print("\n *** Need execute the follow command")
-    print(" ".join(cmd))
